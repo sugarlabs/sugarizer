755190b5679f872a0e6e5c0f233e35ed8e72e6ca
ShashankRajanal7 <rajshash2005@gmail.com>
2025-12-15 14:06:45 +0530

    Add tooltip to Journal icon on home screen

--- js/homeview.js
+++ js/homeview.js
@@ -28,7 +28,10 @@ enyo.kind({
 	kind: enyo.Control,
 	components: [
 		{name: "owner", kind: "Sugar.Icon", size: constant.sizeOwner, colorized: true, classes: "owner-icon", showing: false},
-		{name: "journal", kind: "Sugar.Icon", size: constant.sizeJournal, ontap: "showJournal", classes: "journal-icon", showing: false},
+		{name: "journal", kind: "Sugar.Icon", size: constant.sizeJournal, ontap: "showJournal", classes: "journal-icon", showing: false, attributes: {
+    title: "Journal",
+    "aria-label": "Journal"
+  }},
 		{name: "desktop", showing: true, onresize: "resize", components: []},
 		{name: "otherview", showing: true, components: []},
 		{name: "activityPopup", kind: "Sugar.Popup", showing: false}
